e49ffdbcbbbc7436a572257693ce5ef3508c6698
kash!sh <singhkashish14203@gmail.com>
2024-02-13 21:36:54 +0530

    6thone

--- src/features/product-list/productAPI.js
+++ src/features/product-list/productAPI.js
@@ -1,8 +1,48 @@
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) =>{
-    const response = await fetch('http://localhost:8080')
-const data = await response.json()
-resolve({data})
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products') 
+    const data = await response.json()
+    resolve({data})
   }
-  )
+  );
 }
+
+export function fetchProductsByFilters(filter) {
+  // filter = {"category":"smartphone"}
+  // TODO : on server we will support multi values
+  let queryString = '';
+  for(let key in filter){
+    queryString += `${key}=${filter[key]}&`
+    
+  }
+  
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products?'+queryString) 
+    const data = await response.json()
+    resolve({data})
+  }
+  );
+}
+
+
+
+
+
+
+
+// export const fetchAllProducts = async () => {
+//   try {
+//     const response = await fetch('http://localhost:8080/products');
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error('Error fetching data:', error.message);
+//     throw error; // Rethrow the error to be caught in the component
+//   }
+// };
+
